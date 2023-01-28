6df951e73a017f2018b4c823b6f7a5f419b5f343
WDwelle <willdwelle@frontier.com>
2023-01-27 18:06:59 -0800

    fixing files

---
+++ inventory/src/main.rs
@@ -0,0 +1,11 @@
+//goals:
+//have both backpack and hotbar system
+//be able to swap items in and out of slots
+//possibly add different kind of items that correspond to certain slots ie:
+//armor, weapon, throwable
+
+
+
+fn main() {
+    println!("Hello, world!");
+}
